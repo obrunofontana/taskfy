9d4da90dbd2e7f21150c20205fb5ee17799e7e52
Bruno Fontana <obrunofontana@gmail.com>
2023-12-02 14:50:16 -0300

    feat: create server action to create list

---
+++ actions/create-list/index.ts
@@ -0,0 +1,72 @@
+'use server';
+
+import { auth } from '@clerk/nextjs';
+import { ACTION, ENTITY_TYPE } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+
+import { createAuditLog } from '@/lib/create-audit-log';
+import { createSafeAction } from '@/lib/create-safe-action';
+import { db } from '@/lib/db';
+
+import { CreateList } from './schema';
+import { InputType, ReturnType } from './types';
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { title, boardId } = data;
+  let list;
+
+  try {
+    const board = await db.board.findUnique({
+      where: {
+        id: boardId,
+        orgId,
+      },
+    });
+
+    if (!board) {
+      return {
+        error: 'Board not found',
+      };
+    }
+
+    const lastList = await db.list.findFirst({
+      where: { boardId: boardId },
+      orderBy: { order: 'desc' },
+      select: { order: true },
+    });
+
+    const newOrder = lastList ? lastList.order + 1 : 1;
+
+    list = await db.list.create({
+      data: {
+        title,
+        boardId,
+        order: newOrder,
+      },
+    });
+
+    await createAuditLog({
+      entityTitle: list.title,
+      entityId: list.id,
+      entityType: ENTITY_TYPE.LIST,
+      action: ACTION.CREATE,
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to create.',
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: list };
+};
+
+export const createList = createSafeAction(CreateList, handler);
