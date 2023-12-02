7ecbe33921117c0c32d6979197e2db4d67ef9c8e
Bruno Fontana <obrunofontana@gmail.com>
2023-12-02 14:48:33 -0300

    feat: create server action to delete card

---
+++ actions/delete-card/index.ts
@@ -0,0 +1,54 @@
+'use server';
+
+import { auth } from '@clerk/nextjs';
+import { ACTION, ENTITY_TYPE } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+
+import { createAuditLog } from '@/lib/create-audit-log';
+import { createSafeAction } from '@/lib/create-safe-action';
+import { db } from '@/lib/db';
+
+import { DeleteCard } from './schema';
+import { InputType, ReturnType } from './types';
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { id, boardId } = data;
+  let card;
+
+  try {
+    card = await db.card.delete({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+    });
+
+    await createAuditLog({
+      entityTitle: card.title,
+      entityId: card.id,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.DELETE,
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to delete.',
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const deleteCard = createSafeAction(DeleteCard, handler);
