a0f546c0171cff999912928a8d92be4d92c3fdae
Bruno Fontana <obrunofontana@gmail.com>
2023-12-02 14:50:42 -0300

    feat: create server action to update list

---
+++ actions/update-list/index.ts
@@ -0,0 +1,56 @@
+'use server';
+
+import { auth } from '@clerk/nextjs';
+import { ACTION, ENTITY_TYPE } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+
+import { createAuditLog } from '@/lib/create-audit-log';
+import { createSafeAction } from '@/lib/create-safe-action';
+import { db } from '@/lib/db';
+
+import { UpdateList } from './schema';
+import { InputType, ReturnType } from './types';
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { title, id, boardId } = data;
+  let list;
+
+  try {
+    list = await db.list.update({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+      data: {
+        title,
+      },
+    });
+
+    await createAuditLog({
+      entityTitle: list.title,
+      entityId: list.id,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.UPDATE,
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to update.',
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: list };
+};
+
+export const updateList = createSafeAction(UpdateList, handler);
