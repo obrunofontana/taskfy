d93eca1b7d8a7f87c1550ad614ae4de28d7fb09a
Bruno Fontana <obrunofontana@gmail.com>
2023-12-02 14:48:44 -0300

    feat: create server action to update card

---
+++ actions/update-card/index.ts
@@ -0,0 +1,57 @@
+'use server';
+
+import { auth } from '@clerk/nextjs';
+import { ACTION, ENTITY_TYPE } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+
+import { createAuditLog } from '@/lib/create-audit-log';
+import { createSafeAction } from '@/lib/create-safe-action';
+import { db } from '@/lib/db';
+
+import { UpdateCard } from './schema';
+import { InputType, ReturnType } from './types';
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { id, boardId, ...values } = data;
+  let card;
+
+  try {
+    card = await db.card.update({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    await createAuditLog({
+      entityTitle: card.title,
+      entityId: card.id,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.UPDATE,
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to update.',
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const updateCard = createSafeAction(UpdateCard, handler);
